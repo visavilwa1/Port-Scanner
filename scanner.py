c88a4afde365773bbb868f59c85b0bbc29ba6c01
visavilwa1 <daisyvisavilwa1@gmail.com>
2024-01-17 22:26:04 +0300

    commit

--- scanner.py
+++ scanner.py
@@ -1,1 +1,11 @@
 #!/bin/python3
+import sys
+import socket 
+from datetime import datetime
+
+#Define Our target
+if len (sys.argv) == 2:
+	target = socket.gethostbyname(sys.argv[1]) # Translate hostname to IPV4
+else: 
+	print ("Invalid amount of arguments.")
+	print("Syntax: python3 scanner.py <ip>")
