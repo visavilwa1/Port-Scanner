101a07c3bf4991be17eb6e9e210f1f0dc46640d0
Visavilwa Daisy <135525440+visavilwa1@users.noreply.github.com>
2024-01-22 15:56:11 +0300

    commit

--- scanner.py
+++ scanner.py
@@ -10,3 +10,8 @@ else:
 	print ("Invalid amount of arguments.")
 	print("Syntax: python3 scanner.py <ip>")
 
+# Add a banner
+print("-" * 50)
+print(f"Scanning target: {target}")
+print(f"Time started: {str(datetime.now())}")
+print("-" * 50)
