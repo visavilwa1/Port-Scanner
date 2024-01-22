8384cf9441a6a3c2e89718e0e87a43cf17b48939
visavilwa1 <daisyvisavilwa1@gmail.com>
2024-01-22 16:44:37 +0300

    commit

--- scanner.py
+++ scanner.py
@@ -15,3 +15,21 @@ print("-" * 50)
 print(f"Scanning target: {target}")
 print(f"Time started: {str(datetime.now())}")
 print("-" * 50)
+
+try:
+	for port in range (50,85):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.setdefaulttimeout(1)
+		result = s. connect_ex((target,port))
+		if result == 0:
+			print(f"Port {port} is open")
+		s.close() 
+except KeyboardInterrupt:
+	print("\nExiting program.")
+	sys.exit()
+except socket.gaierror:
+	print("Hostname could not be resolved.")
+	sys.exit()
+except socket.error:
+	print("Could not connect to server.")
+	sys.exit()
